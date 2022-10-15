b2486968b54ae1350c728c214e069f2d6a4c0188
michaelfarrell76 <mike@transcend.io>
2022-10-15 14:53:11 -0700

    Adds boilerplate to repo

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import * as t from 'io-ts';
+
+export const TEST = t.type({});
+
+// Code will come in follow up PR
+export const INIT = 0;
---
+++ src/tests/PersistedState.test.ts
@@ -0,0 +1,8 @@
+// external
+import { expect } from 'chai';
+
+describe('PesistedState', () => {
+  it('tests will come in follow up PR', () => {
+    expect(1).to.equal(2);
+  });
+});
